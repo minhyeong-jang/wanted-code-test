65ecfe2e945e61b0d8693d8051a32e9b2937f15b
noah <noah@dunamu.com>
2019-11-29 17:39:14 +0900

    Updates

---
+++ precache-manifest.e9e4cb36da21ffdb4c8995cd5e98c979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b7f29704fc61cbf1df",
+    "url": "/wanted-code-test/static/js/runtime~main.33b7f297.js"
+  },
+  {
+    "revision": "273f7d0891569307a2d2",
+    "url": "/wanted-code-test/static/js/main.273f7d08.chunk.js"
+  },
+  {
+    "revision": "c099f14f37673b4e08f7",
+    "url": "/wanted-code-test/static/js/1.c099f14f.chunk.js"
+  },
+  {
+    "revision": "273f7d0891569307a2d2",
+    "url": "/wanted-code-test/static/css/main.8a182f12.chunk.css"
+  },
+  {
+    "revision": "68c2d82c92b619c9780bf6d80cc7670e",
+    "url": "/wanted-code-test/index.html"
+  }
+];
